4c24892ec838c1e33b4509023817c779c53fd970
amarjvr <amarjvr10@gmail.com>
2021-11-07 21:06:28 +0530

    this is bhargav

---
+++ src/main/java/log4j/bhargav22/bhargav.java
@@ -0,0 +1,15 @@
+package log4j.bhargav22;
+
+import java.util.logging.Logger;
+
+public class bhargav {
+	private static Logger logger=Logger.getLogger("bhargav.class");
+	public static void main(String[] args) {
+		for(int i=0;i<100;i++) {
+			Logger.d
+			
+	}
+		
+	}
+
+}
